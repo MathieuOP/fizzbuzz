52c5b1a61dece8d9ff2772a9bce2068690a0099f
Mathieu <mathieuoliveirapereira@gmail.com>
2022-05-09 15:47:32 +0200

    modification of the test title

--- fizzbuzz.test.ts
+++ fizzbuzz.test.ts
@@ -1,7 +1,7 @@
 import { fizzbuzz } from './fizzbuzz';
 
 describe('Fizzbuzz', () => {
-  test('fizzbuzz function should return a number', () => {
+  test('Should return a number when the number is not a multiple of 3, 5 or 15', () => {
     const number = 1;
     const result = fizzbuzz(number);
     const resultExpected = 1;
