9589d14b75f08a83f7d3b2e455242984eb7fdf86
Mathieu <mathieuoliveirapereira@gmail.com>
2022-05-12 14:51:14 +0200

    refactoring of the check if is number is a multiple of

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,3 +1,5 @@
+const isAMultpleOf = (number: number, multiplier: number): boolean => number % multiplier === 0;
+
 export const fizzbuzz = (number: number): string | number => {
   const MINIMUM_NUMBER_ALLOWED = 1;
   const MAXIMUM_NUMBER_ALLOWED = 100;
@@ -8,9 +10,9 @@ export const fizzbuzz = (number: number): string | number => {
   if (number < MINIMUM_NUMBER_ALLOWED || number > MAXIMUM_NUMBER_ALLOWED) {
     return "Please enter a number between 1 and 100";
   }
-  if (number % FIZZBUZZ_MULTIPLIER === 0) return "fizzbuzz";
-  if (number % BUZZ_MULTIPLIER === 0) return "buzz";
-  if (number % FIZZ_MULTIPLIER === 0) return "fizz";
   
+  if (isAMultpleOf(number, FIZZBUZZ_MULTIPLIER)) return "fizzbuzz";
+  if (isAMultpleOf(number, BUZZ_MULTIPLIER)) return "buzz";
+  if (isAMultpleOf(number, FIZZ_MULTIPLIER)) return "fizz";
   return number;
 };
