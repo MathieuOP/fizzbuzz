65192fab1888db617fa23dbab0bda49b0b94e557
Mathieu <mathieuoliveirapereira@gmail.com>
2022-05-12 14:47:16 +0200

    replace all magic values by constante

--- fizzbuzz.ts
+++ fizzbuzz.ts
@@ -1,7 +1,16 @@
 export const fizzbuzz = (number: number): string | number => {
-  if (number < 1 || number > 100) return "Please enter a number between 1 and 100";
-  if (number % 15 === 0) return "fizzbuzz";
-  if (number % 5 === 0) return "buzz";
-  if (number % 3 === 0) return "fizz";
+  const MINIMUM_NUMBER_ALLOWED = 1;
+  const MAXIMUM_NUMBER_ALLOWED = 100;
+  const FIZZ_MULTIPLIER = 3;
+  const BUZZ_MULTIPLIER = 5;
+  const FIZZBUZZ_MULTIPLIER = 15;
+   
+  if (number < MINIMUM_NUMBER_ALLOWED || number > MAXIMUM_NUMBER_ALLOWED) {
+    return "Please enter a number between 1 and 100";
+  }
+  if (number % FIZZBUZZ_MULTIPLIER === 0) return "fizzbuzz";
+  if (number % BUZZ_MULTIPLIER === 0) return "buzz";
+  if (number % FIZZ_MULTIPLIER === 0) return "fizz";
+  
   return number;
 };
